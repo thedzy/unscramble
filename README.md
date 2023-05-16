31dc594249136f3122c1f47190a1c12719837fab
Shane Young <shane.y@nylas.com>
2023-05-15 20:22:45 -0400

    Initial commit
    v1

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# unscramble
- Uncramble letters
+# unscramble.go
+
+Take some letters and find words that can be made with those letters
+
+## What?
+### unscramble.go
+This is my first attempt at Go.  I have no idea of some of the conventions or best practices, so feel free to comment.\ 
+I decided to try and create something similar to my boggle solver, particularily becasue I wanted an easy way to unscramble some words\ 
+...And yes, I know there are utilities, but 1.  Nothing more fun than doing to yourself, 2. I can feed it any words that I want
+
+Example:
+```bash
+% go run /Users/syoung/git2/unscramble/unscramble.go  --letters exmaple --min 5 --max 5
+```
+``` ignorelang
+ Starting
+ Finding words of 5 to 5 length
+ Found 4 words
+ expel
+ maple
+ ample
+ pelma
+ Done
+```
+
+## Why?
+Kinda covered this above, but mostly to try Go
+
+## Improvements?
+Speed wise I thinks its about as fast as it gets.  Going to look at multithreading but I think Go already does that
+
+## State?
+No known bugs.  Works.  
+
+## New
+### 1.0
+- Takes some letter and rearrange them to new words
+- Human readable or json output
---
+++ unscramble.go
@@ -0,0 +1,409 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/akamensky/argparse"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Options struct {
+	ScrambledString string
+	WordFile        os.File
+	Min             int
+	Max             int
+	DebugLevel      int
+	SortMethod      string
+	SortReverse     bool
+	JsonOutput      bool
+}
+
+type Letter struct {
+	Self     string             `json:"self"`
+	Children map[string]*Letter `json:"children,omitempty"`
+}
+
+// Logger
+type Logger struct {
+	*log.Logger
+	level int
+}
+
+func newLogger(prefix string, level int) *Logger {
+	return &Logger{
+		Logger: log.New(os.Stdout, prefix, 0),
+		level:  level,
+	}
+}
+
+func (l *Logger) Log(level int, v ...interface{}) {
+	colour := "\u001B[0;m"
+	switch {
+	case level >= 10 && level < 20:
+		colour = "\u001B[5;90;m"
+	case level >= 20 && level < 30:
+		colour = "\u001B[0;m"
+	case level >= 30 && level < 40:
+		colour = "\u001B[5;32;m"
+	case level >= 40 && level < 50:
+		colour = "\u001B[5;93;m"
+	case level >= 50 && level < 60:
+		colour = "\u001B[5;41;97;m"
+	}
+	endColour := "\u001B[0m"
+
+	if level >= l.level {
+		message := append([]interface{}{colour}, v...)
+		message = append(message, endColour)
+		l.Logger.Println(message...)
+	}
+	return
+}
+
+// Log functions
+func (l *Logger) VerboseDebug(message string, v ...interface{}) {
+	l.Log(10, fmt.Sprintf(message, v...))
+}
+
+func (l *Logger) Debug(message string, v ...interface{}) {
+	l.Log(11, fmt.Sprintf(message, v...))
+}
+
+func (l *Logger) Info(message string, v ...interface{}) {
+	l.Log(20, fmt.Sprintf(message, v...))
+}
+
+func (l *Logger) Warning(message string, v ...interface{}) {
+	l.Log(30, fmt.Sprintf(message, v...))
+}
+
+func (l *Logger) Error(message string, v ...interface{}) {
+	l.Log(40, fmt.Sprintf(message, v...))
+}
+
+func (l *Logger) Critical(message string, v ...interface{}) {
+	l.Log(50, fmt.Sprintf(message, v...))
+	os.Exit(254)
+}
+
+// Main
+func main() {
+
+	// Loaf options and start logging
+	options := getOptions()
+	if options.JsonOutput {
+		options.DebugLevel = 40
+	}
+	logger := newLogger("", options.DebugLevel)
+
+	// Debug the options
+	v := reflect.ValueOf(options)
+	t := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		fieldName := t.Field(i).Name
+		fieldValue := field.Interface()
+		logger.Debug("%s: %v", fieldName, fieldValue)
+	}
+
+	logger.Debug("Debug ON")
+	logger.Info("Starting")
+
+	// Lets track how long tasks take
+	startTime := time.Now()
+
+	// Read the word file content
+	content, err := os.ReadFile(options.WordFile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger.Debug("Loaded file: %s\n", time.Since(startTime))
+
+	// Split the content into lines
+	lines := strings.Split(string(content), "\n")
+
+	// Create a root for the dictionary tree.  Similiar to https://github.com/thedzy/boggle_solver except we generate it live
+	root := &Letter{
+		Children: make(map[string]*Letter),
+	}
+
+	// Load the words into the tree
+	for _, line := range lines {
+		addWord(root, strings.ToLower(line))
+	}
+
+	logger.Debug("Built word tree: %s\n", time.Since(startTime))
+
+	// Debugging the tree
+	jsonBytes, err := json.MarshalIndent(root, "", "  ")
+	if err != nil {
+		logger.Error("Error marshaling JSON:", err)
+		return
+	}
+	logger.VerboseDebug(string(jsonBytes))
+
+	if options.Max == 0 {
+		options.Max = len(options.ScrambledString)
+	}
+	logger.Info("Finding words of %d to %d length", options.Min, options.Max)
+	matches := getVariations(root, strings.ToLower(options.ScrambledString), options.Min, options.Max)
+	logger.Info("Found %d words", len(matches))
+
+	logger.Debug("Found matches: %s\n", time.Since(startTime))
+
+	// Sort the matches
+	if options.SortMethod == "a" || options.SortMethod == "alpha" {
+		sort.Strings(matches)
+	}
+	if options.SortMethod == "l" || options.SortMethod == "len" {
+		sort.Slice(matches, func(i, j int) bool {
+			return len(matches[i]) < len(matches[j])
+		})
+	}
+	if options.SortReverse {
+		// Reverse the order of elements in the list
+		sort.SliceStable(matches, func(i, j int) bool {
+			return i > j
+		})
+	}
+
+	// Print results
+	if options.JsonOutput {
+		jsonData, err := json.Marshal(matches)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Println(string(jsonData))
+	} else {
+		for _, match := range matches {
+			logger.Info(match)
+		}
+	}
+
+	logger.Debug("Printed words: %s\n", time.Since(startTime))
+
+	logger.Info("Done")
+
+}
+
+func addWord(parent *Letter, word string) {
+	// addWord: appends another word to the dictionary tree.
+	//
+	// It follows the tree until a new branch is needed and branches from there
+	// the parent node is the parent from the last branch or the root
+
+	for _, char := range word {
+		letter := string(char)
+
+		if _, exists := parent.Children[letter]; !exists {
+			parent.Children[letter] = &Letter{
+				Self:     letter,
+				Children: make(map[string]*Letter),
+			}
+		}
+
+		parent = parent.Children[letter]
+	}
+}
+
+func searchWord(node *Letter, word string) bool {
+	// searchWord: recursively follows the tree until it can't find a branch.
+	//
+	// This can be used to find partial or complete branches.
+	// Have a terminator to the end of the word differentiates between partial and full
+
+	// If we have exhausted all letters than the search is complete and we have no reached the point where there is no branches left
+	if len(word) == 0 {
+		return true
+	}
+
+	// If the next branch doesn't exist we fail, otherwise we follow the next
+	letter := string(word[0])
+	nextNode, exists := node.Children[letter]
+	if !exists {
+		return false
+	}
+
+	return searchWord(nextNode, word[1:])
+}
+
+func getVariations(node *Letter, letters string, min int, max int) []string {
+	// getVariations: acts like a container to hold the results from the searches.
+	//
+	// It follows the tree untill a new branch is needed and branchss from there
+	// the parent node is the parent from the last branch or the root
+	combinations := []string{}
+	used := make([]bool, len(letters))
+	builder := strings.Builder{}
+
+	searchVarient(node, letters, used, &builder, &combinations, min, max)
+
+	return combinations
+}
+
+func searchVarient(node *Letter, letters string, used []bool, builder *strings.Builder, combinations *[]string, min int, max int) {
+	// searchVarient: is where I got the idea for this, I was looking to create something like th boggle solver in go
+	//
+	// It takes the first letters and then starts a process for each subsequent letter until we hit the end
+
+	// If the word is in range and if it can be matched fully
+	if builder.Len() >= min && builder.Len() <= max {
+		if searchWord(node, builder.String()+"\r") {
+			if !inList(*combinations, builder.String()) {
+				*combinations = append(*combinations, builder.String())
+			}
+		}
+		//return
+	}
+
+	// Start looping throught he first letters
+	for i := 0; i < len(letters); i++ {
+		if used[i] {
+			continue
+		}
+
+		used[i] = true
+		builder.WriteByte(letters[i])
+
+		// Keep checking that the word can possibly match
+		// Speeds up search byt not following words that will never be words
+		if searchWord(node, builder.String()) {
+			//print(builder.String() + "\n")
+			searchVarient(node, letters, used, builder, combinations, min, max)
+		}
+
+		used[i] = false
+		newStr := builder.String()[:builder.Len()-1]
+		builder.Reset()
+		builder.WriteString(newStr)
+	}
+}
+
+func inList(list []string, str string) bool {
+	// inList: is the word already in a list/
+	//
+	// I could find a way to do this natively like in Python, it checks that a word is not already in a list
+	for _, item := range list {
+		if item == str {
+			return true
+		}
+	}
+	return false
+}
+
+func getOptions() Options {
+	// getOptions: get the program options.  If any one was a better way, please comment
+	//
+	// I like the way python handles options and this is the best I could find without essentially recreating this
+
+	// Get path for options
+	dir, err := filepath.Abs(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Options
+	parser := argparse.NewParser("unscramble", "take some letters and arrange them in different orders and find the words")
+	var version = parser.Flag(
+		"v", "version",
+		&argparse.Options{
+			Help: "Current version"})
+
+	// Input options
+	var scrambledString *string = parser.String("l", "letters",
+		&argparse.Options{
+			Help: "Letters ",
+			Validate: func(args []string) error {
+				for _, arg := range args {
+					if match, _ := regexp.MatchString("^[A-z]+$", arg); !match {
+						return fmt.Errorf("number must be between A and Z")
+					}
+				}
+				return nil
+			},
+			Required: true})
+
+	var WordFile *os.File = parser.File("f", "file", os.O_RDWR, 0600,
+		&argparse.Options{
+			Help:    "Words file ",
+			Default: filepath.Join(dir, "collins_scrabble_words_2019.txt")})
+
+	// Output options
+	var SortMethod *string = parser.Selector("s", "sort", []string{"a", "alpha", "l", "len"},
+		&argparse.Options{
+			Help:    "Sorting method ",
+			Default: nil})
+
+	var SortReverse *bool = parser.Flag("r", "sort-reverse",
+		&argparse.Options{
+			Help:    "Sort reversed",
+			Default: false,
+		})
+
+	var Min *int = parser.Int("", "min",
+		&argparse.Options{
+			Help:    "Length of the smallest word",
+			Default: 0})
+
+	var Max *int = parser.Int("", "max",
+		&argparse.Options{
+			Help:    "Length of the largest word",
+			Default: nil})
+
+	var DebugLevel *int = parser.Int("", "log-level",
+		&argparse.Options{
+			Help: "Set the logging level",
+			Validate: func(args []string) error {
+				for _, arg := range args {
+					number, err := strconv.Atoi(arg)
+					if err != nil {
+						return fmt.Errorf("invalid number: %s", arg)
+					}
+					if number < 10 || number > 60 {
+						return fmt.Errorf("number must be between 10 and 60")
+					}
+				}
+				return nil
+			},
+			Default: 20,
+		})
+
+	var JsonOutput *bool = parser.Flag("j", "json",
+		&argparse.Options{
+			Help:    "Json output",
+			Default: false})
+
+	// Parsing options
+	err = parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		os.Exit(1)
+	}
+
+	// Version
+	if *version {
+		print("1.0", "\n")
+		os.Exit(0)
+	}
+
+	return Options{
+		ScrambledString: *scrambledString,
+		WordFile:        *WordFile,
+		Min:             *Min,
+		Max:             *Max,
+		DebugLevel:      *DebugLevel,
+		SortMethod:      *SortMethod,
+		SortReverse:     *SortReverse,
+		JsonOutput:      *JsonOutput,
+	}
+}
